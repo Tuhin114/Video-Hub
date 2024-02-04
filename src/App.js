6ce9eba6b966e7ba542f8e54c84c6a1fd1d6a199
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-04 21:56:08 +0530

    Basic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Button } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return;
+  return (
+    <Router>
+      <div>Hello</div>
+      <Button colorScheme="whatsapp">Click</Button>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
